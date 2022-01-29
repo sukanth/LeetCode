2d67e49b6b1b8ba57f0d73ca62b5524c04ddc4ab
Sukanth Gunda <contact.sukanth@gmail.com>
2022-01-29 00:03:22 -0600

    new changes

--- src/main/java/com/sukanth/MergeTwoSortedArrays.java
+++ src/main/java/com/sukanth/MergeTwoSortedArrays.java
@@ -3,40 +3,60 @@ package com.sukanth;
 import java.util.Arrays;
 
 public class MergeTwoSortedArrays {
-    public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {3, 4, 5};
-        final int[] ints = mergeTwoSortedArrays(nums1, 3, nums2, 3);
-        Arrays.stream(ints).forEach(s -> System.out.println(s));
-        
+  public static void main(String[] args) {
+    int[] nums1 = {1, 2, 3, 0, 0, 0};
+    int[] nums2 = {3, 4, 5};
+      int[] nums3 = {1, 2, 3};
+      int[] nums4 = {3, 4, 5};
+    final int[] ints = mergeTwoSortedArrays(nums1, 3, nums2, 3);
+    int[] result = mergeTwoSortedArrays(nums3, nums4);
+    Arrays.stream(ints).forEach(s -> System.out.println(s));
+  }
+
+  /**
+   * @param nums1
+   * @param m
+   * @param nums2
+   * @param n
+   */
+  private static int[] mergeTwoSortedArrays(int[] nums1, int m, int[] nums2, int n) {
+    // copy nums1 to nums1copy
+    int[] nums1copy = new int[m];
+    for (int i = 0; i < m; i++) {
+      nums1copy[i] = nums1[i];
     }
 
-    /**
-     *
-     * @param nums1
-     * @param m
-     * @param nums2
-     * @param n
-     */
-    private static int[] mergeTwoSortedArrays(int[] nums1, int m, int[] nums2, int n) {
-        //copy nums1 to nums1copy
-        int[] nums1copy = new int[m];
-        for (int i = 0; i < m; i++) {
-            nums1copy[i] = nums1[i];
-        }
+    // initialize two read pointers and one write pointer
 
-        //initialize two read pointers and one write pointer
+    int p1 = 0;
+    int p2 = 0;
 
-        int p1 = 0;
-        int p2 = 0;
-
-        for (int p = 0; p < m + n; p++) {
-            if (p2>=n || p1<m && nums1copy[p1]<nums2[p2]) {
-                nums1[p] = nums1copy[p1++];
-            } else {
-                nums1[p] = nums2[p2++];
-            }
-        }
-        return nums1;
+    for (int p = 0; p < m + n; p++) {
+      if (p2 >= n || p1 < m && nums1copy[p1] < nums2[p2]) {
+        nums1[p] = nums1copy[p1++];
+      } else {
+        nums1[p] = nums2[p2++];
+      }
     }
+    return nums1;
+  }
+
+  private static int[] mergeTwoSortedArrays(int[] nums1, int nums2[]) {
+    int m = nums1.length;
+    int n = nums2.length;
+
+    int[] result = new int[m + n];
+
+    int p1 = 0;
+    int p2 = 0;
+
+    for (int p = 0; p < m + n; p++) {
+      if (p2 >= n || p1 < m && nums1[p1] < nums2[p2]) {
+        result[p] = nums1[p1++];
+      } else {
+        result[p] = nums2[p2++];
+      }
+    }
+    return result;
+  }
 }
