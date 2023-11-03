f166c841d0f74ae1f1f15d57a5a7b28621d014f8
Sukanth Gunda <contact.sukanth@gmail.com>
2023-11-03 15:50:27 -0500

    contains duplicate added leetcode easy

---
+++ src/main/java/com/sukanth/arraysandhashset/ContainsDuplicate.java
@@ -0,0 +1,30 @@
+package com.sukanth.arraysandhashset;
+
+import java.util.HashSet;
+
+/**
+ * Question : https://leetcode.com/problems/contains-duplicate/
+ * Solution : https://github.com/neetcode-gh/leetcode/blob/main/java/0217-contains-duplicate.java
+ * Explanation : https://www.youtube.com/watch?v=3OamzN90kPg
+ *
+ * Time Complexity : O(n)
+ * Space Complexity : O(n)
+ *
+ */
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3,4,5,6};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> uniqueNums = new HashSet<>();
+        for (Integer num: nums) {
+            if(uniqueNums.contains(num)){
+                return true;
+            }
+            uniqueNums.add(num);
+        }
+        return false;
+    }
+}
